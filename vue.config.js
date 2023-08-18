2efa495b3e9bf7edd83e4a9892ec75fa27260b6a
Petro <110422807+Killgf@users.noreply.github.com>
2023-08-18 18:31:41 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'best-team/docs/'  // Шлях до index.html у відношенні до кореня сайту
-    : '/'
-}
-
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
