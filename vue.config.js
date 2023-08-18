5e257c59084a6c1f5f04e2cd44ec681b09b67ba4
Petro <110422807+Killgf@users.noreply.github.com>
2023-08-18 16:10:30 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
 })
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/best-team/public/index.html'  // Замініть 'repository-name' на назву вашого репозиторію
+    ? '/best-team/public/'  // Замініть 'repository-name' на назву вашого репозиторію
     : '/'
 }
